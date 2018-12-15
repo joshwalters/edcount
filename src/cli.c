5b563c77139155a3d5ec517f633cb671c5b58fe7
Harold Freeman <haroldfreeman@protonmail.com>
2018-12-15 01:07:54 -0800

    Bumped version to 0.1.1, not yet ready for release however.

--- src/cli.c
+++ src/cli.c
@@ -85,6 +85,11 @@ parse_args(int argc, char **argv, struct CLIArgs *cli_args)
         print_version(argv);
         exit(EXIT_SUCCESS);
     }
+    // Super secret flag
+    if (is_flag_present(argc, argv, "--voncount", "--voncount")) {
+        fprintf(stdout, "Seven! Seven bats! Ah, Ah Ah!\n");
+        exit(EXIT_SUCCESS);
+    }
     // Check for unknown flags, report to user and fail if found
     int i;
     for (i = 1; i < argc; i++) {
